dbb1ef3d35f6ef9ce6cb1b6c03f2f162348c4fd8
Svetlana <angel_forever99@mail.ru>
2022-12-27 19:11:11 +0300

    new commit lesson 7

---
+++ Task47/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+//получить размерность массива с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+//инициализация элементов матрицы случайными числами
+double[,] InitMatrix(double n, double m)
+{
+    double[,] matrix = new double[n, m];
+    Random rnd = new Random();
+    for (double i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (double j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+//распечатать массив на консоль
+void PrintMatrix(double[,] matrix)
+{
+    for (double i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (double j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+double n = GetNumber("Введите количество строк:");
+double m = GetNumber("Введите количество столбцов:");
+double[,] matrix = InitMatrix(n, m);
+PrintMatrix(matrix);
+
+
---
+++ Task50/Program.cs
@@ -0,0 +1,77 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает 
+значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет*/
+
+//получить размерность массива с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+//инициализация элементов матрицы случайными числами
+int[,] InitMatrix(int n, int m)
+{
+    int[,] matrix = new int[n, m];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+//распечатать массив на консоль
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+int n = GetNumber("Введите кол-во строк: ");
+int m = GetNumber("Введите кол-во столбцов: ");
+
+int[,] matrix = InitMatrix(n, m);
+
+int NPosition = GetNumber("Введите позицию строки: ");
+int MPosition = GetNumber("Введите позицию столбца: ");
+
+if (NPosition > matrix.GetLength(0) - 1 || MPosition > matrix.GetLength(1) - 1)
+    Console.WriteLine("такой позиции в массиве нет!");
+else
+{
+    PrintMatrix(matrix);
+    Console.WriteLine($"Значение элемента [{NPosition};{MPosition}] = {matrix[NPosition, MPosition]}");
+}
